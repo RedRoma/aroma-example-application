a54d09502299f65f6c1f4a050d29065fd9aaecaf
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-03-18 11:37:45 -0700

    Updating App Token

--- src/main/java/tech/aroma/example/Main.java
+++ src/main/java/tech/aroma/example/Main.java
@@ -61,7 +61,7 @@ public class Main
     private static final ScheduledExecutorService EXECUTOR = Executors.newScheduledThreadPool(2);
 
     private static final String APP_ID = "5b7833c5-d3dc-4b6b-a29c-ba2a9dddba35";
-    private static final String APP_TOKEN = "54663d40-d058-4fec-ac86-f37e75170f16";
+    private static final String APP_TOKEN = "796fd6d8-538e-4e3b-964e-506cf57cbf39";
 
     private static final AlchemyGenerator<String> TITLES = StringGenerators.stringsFromFixedList(
         "App Crashed",
