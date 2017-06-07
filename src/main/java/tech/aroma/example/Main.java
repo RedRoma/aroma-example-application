19658caa4901098673ae995724fee3e1e7d5747d
Wellington Moreno <jwellington.moreno@gmail.com>
2017-06-07 12:32:31 -0700

    Updates for JDK 1.7

--- src/main/java/tech/aroma/example/Main.java
+++ src/main/java/tech/aroma/example/Main.java
@@ -18,25 +18,23 @@ package tech.aroma.example;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.aroma.client.Aroma;
-import tech.aroma.client.Urgency;
+import tech.aroma.client.Priority;
 import tech.aroma.thrift.application.service.ApplicationServiceConstants;
 import tech.aroma.thrift.endpoint.TcpEndpoint;
 import tech.sirwellington.alchemy.generator.AlchemyGenerator;
 import tech.sirwellington.alchemy.generator.StringGenerators;
 
-import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.generator.AlchemyGenerator.Get.one;
 import static tech.sirwellington.alchemy.generator.EnumGenerators.enumValueOf;
-import static tech.sirwellington.alchemy.generator.StringGenerators.alphabeticString;
+import static tech.sirwellington.alchemy.generator.StringGenerators.alphabeticStrings;
 
 /**
- *
  * @author SirWellington
  */
 public class Main
@@ -51,36 +49,39 @@ public class Main
     private static final String APP_TOKEN = "ec033ff6-af81-4678-8a79-a4c4aa8a37fc";
 
     private static final AlchemyGenerator<String> TITLES = StringGenerators.stringsFromFixedList(
-        "App Crashed",
-        "App Launched",
-        "App Updated",
-        "Network Issue",
-        "New User",
-        "Build Failed",
-        "Token Expired",
-        "Disk Full",
-        "User Deleted Account",
-        "Invalid Credit Card Used",
-        "Database Time Out",
-        "Could not reach Authentication Service",
-        "Could not reach Twilio",
-        "Unauthorized Action",
-        "Service Redeployed",
-        "Database Query Failed",
-        "Device Lost Connection",
-        "Battery Low",
-        "Alarm Triggered",
-        "User Bought Item");
+            "App Crashed",
+            "App Launched",
+            "App Updated",
+            "Network Issue",
+            "New User",
+            "Build Failed",
+            "Token Expired",
+            "Disk Full",
+            "User Deleted Account",
+            "Invalid Credit Card Used",
+            "Database Time Out",
+            "Could not reach Authentication Service",
+            "Could not reach Twilio",
+            "Unauthorized Action",
+            "Service Redeployed",
+            "Database Query Failed",
+            "Device Lost Connection",
+            "Battery Low",
+            "Alarm Triggered",
+            "User Bought Item");
 
-    private static final AlchemyGenerator<Urgency> PRIORITIES = enumValueOf(Urgency.class);
+    private static final AlchemyGenerator<Priority> PRIORITIES = enumValueOf(Priority.class);
 
-    private static final TcpEndpoint ENDPOINT = ApplicationServiceConstants.PRODUCTION_ENDPOINT;
+    private static final TcpEndpoint LOCAL = new TcpEndpoint().setHostname("localhost").setPort(7002);
+    private static final TcpEndpoint PRODUCTION = ApplicationServiceConstants.PRODUCTION_ENDPOINT;
+    private static final TcpEndpoint ENDPOINT = PRODUCTION;
 
-    private static final Aroma AROMA = Aroma.newBuilder()
-        .withEndpoint(ENDPOINT.hostname, ENDPOINT.port)
-        .withApplicationToken(APP_TOKEN)
-        .withAsyncExecutorService(Executors.newSingleThreadExecutor())
-        .build();
+    private static final Aroma AROMA = Aroma.Builder
+            .create()
+            .withEndpoint(ENDPOINT.hostname, ENDPOINT.port)
+            .withApplicationToken(APP_TOKEN)
+            .withAsyncExecutorService(Executors.newSingleThreadExecutor())
+            .build();
 
     public static void main(String[] args) throws IOException, TException
     {
@@ -90,16 +91,32 @@ public class Main
     private static void startApp() throws IOException
     {
         openPortAt(PORT);
-       
-        EXECUTOR.scheduleAtFixedRate(Main::sendMessage, 2000, 2000, TimeUnit.MILLISECONDS);
+
+        EXECUTOR.scheduleAtFixedRate(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                sendMessage();
+            }
+        }, 2000, 2000, TimeUnit.MILLISECONDS);
     }
 
     private static void openPortAt(int port) throws IOException
     {
         LOG.info("Opening port at {}", port);
 
-        ServerSocket socket = new ServerSocket(port);
-        EXECUTOR.submit(() -> socket.accept());
+        final ServerSocket socket = new ServerSocket(port);
+
+        EXECUTOR.submit(new Callable<Void>()
+        {
+            @Override
+            public Void call() throws IOException
+            {
+                socket.accept();
+                return null;
+            }
+        });
 
         LOG.info("Opened port at {}", port);
 
@@ -109,14 +126,14 @@ public class Main
     {
         LOG.info("Sending Message");
         String title = one(TITLES);
-        String randomMessage = one(alphabeticString(100));
-        Urgency urgency = one(PRIORITIES);
+        String randomMessage = one(alphabeticStrings(100));
+        Priority urgency = one(PRIORITIES);
 
         AROMA.begin()
-            .titled(title)
-            .text(randomMessage)
-            .withUrgency(urgency)
-            .send();
+             .titled(title)
+             .withBody(randomMessage)
+             .withPriority(urgency)
+             .send();
     }
 
 }
